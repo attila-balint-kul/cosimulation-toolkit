0037c4fd925b88ee0b4cd7d918be156e33092de2
Attila Balint <attila.balint@kuleuven.be>
2023-08-04 12:39:33 +0200

    first commit

---
+++ examples/simulation.py
@@ -0,0 +1,22 @@
+from cosimtlk.simulator import FMUSimulator
+
+
+def main():
+    with FMUSimulator("fmus/RealBoolIntTest.fmu") as simulator:
+        print(simulator.info())
+        print(simulator.inputs())
+        print(simulator.outputs())
+        print(simulator.parameters())
+        print(simulator.step(input_int=2, input_float=3.16, input_bool=True))
+        print(simulator.step(input_int=3, input_float=2.16, input_bool=False))
+        print(simulator.step(input_int=4, input_float=1.16, input_bool=True))
+        simulator.change_parameters(**{"gain.k": -2.0})
+        print(simulator.parameters())
+        print(simulator.step(n=2, input_int=5, input_float=2.16, input_bool=False))
+        print(simulator.step(n=2, input_int=6, input_float=3.16, input_bool=True))
+        print(simulator.step(n=2, input_int=7, input_float=4.16, input_bool=False))
+        print(simulator.advance(until=15))
+
+
+if __name__ == "__main__":
+    main()
